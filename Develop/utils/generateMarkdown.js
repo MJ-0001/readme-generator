8fba2ce9d146e9b639307f22a0c3b1a1921ed505
Matthew Jones <mejones92@gmail.com>
2021-02-20 20:53:44 +0000

    Conditional statements added to check what license a user has specified, so that the relevant license badge and link can be generated.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,53 +1,103 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to generate the markdown file
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // Returns a license badge based on the license passed in
+  function licenseBadge() {
+    if (data.license == 'Apache') {
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    }
+    else if (data.license == 'MIT') {
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    }
+    else if (data.license == 'BSD 2') {
+      return '![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)';
+    }
+    else if (data.license == 'BSD 3') {
+      return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    }
+    else if (data.license == 'none') {
+      return ''
+    }
+  }
+  
+  // Returns link to further license information
+  function licenseLink() {
+    if (data.license == 'Apache') {
+      return 'https://opensource.org/licenses/Apache-2.0';
+    }
+    else if (data.license == 'MIT') {
+      return 'https://opensource.org/licenses/MIT';
+    }
+    else if (data.license == 'BSD 2') {
+      return 'https://opensource.org/licenses/BSD-2-Clause';
+    }
+    else if (data.license == 'BSD 3') {
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    }
+    else if (data.license == 'none') {
+      return ''
+    }
+  }
+
+  return `
+  # ${data.title}
+
   # Table of Contents
-  - [description](#description)
-  - [install](#install)
-  - [instructions](#instructions)
-  - [credits](#credits)
-  - [license](#license)
-  - [contributions](#contributions)
-  - [tests](#tests)
+  - [Description](#description)
+  - [Installation](#install)
+  - [Instructions](#instructions)
+  - [Credits](#credits)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+  ---
 
   ## Description:
+  
   ${data.description}
+  
+  ---
 
   ## Installation:
   ${data.install}
 
+  ---
   ## Instructions for use:
   ${data.instructions}
 
+  ---
   ## Credits:
   ${data.credits}
 
-  ## License:
-  ![https://]${data.license}
-
+  ---
   ## Contributions:
   ${data.contributions}
 
+  ---
   ## Tests:
+  
   ${data.tests}
-
-
   
-  
-  `;
+  ---
+  ## Questions:  
+  If you have any questions please find my Github username and my email address below:  
+
+  * ${data.git}
+  * ${data.email}
+
+  ---
+  ## License:  
+
+  ${data.license}  
+
+  ${licenseBadge()}  
+
+  ${licenseLink()}
+ 
+  ---`;
 
 }
 
 module.exports = generateMarkdown;
+
+
